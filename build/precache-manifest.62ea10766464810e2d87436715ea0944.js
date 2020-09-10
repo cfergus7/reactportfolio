61d149c5cfd7cc41c250e7d6c43e075726cb31fd
Chris Ferguson <cfergus7@gmail.com>
2020-09-10 12:00:09 -0500

    brick by brick

---
+++ build/precache-manifest.62ea10766464810e2d87436715ea0944.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da92455ca3900424d0d2ea7db534b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe78459a7150ecdaab0",
+    "url": "/static/css/main.ab93fd3a.chunk.css"
+  },
+  {
+    "revision": "5a3ea32d20083bca28e0",
+    "url": "/static/js/2.7cce064a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7cce064a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe78459a7150ecdaab0",
+    "url": "/static/js/main.55fbb56e.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "5f98b4b52697ffddffdb65a8b6a80b4a",
+    "url": "/static/media/budgetCalculatorThumbnail.5f98b4b5.png"
+  },
+  {
+    "revision": "d9823985fab21e6b659171a1bfa8a3ff",
+    "url": "/static/media/eatDaBurgerThumbnail.d9823985.png"
+  },
+  {
+    "revision": "bcd70a0765e62d3b7c0fe3d6d8b56f12",
+    "url": "/static/media/firstImpressionThumbnail.bcd70a07.png"
+  },
+  {
+    "revision": "1eb2a39069d4fe017e12f9b976380061",
+    "url": "/static/media/fitnessTrackerThumbnail.1eb2a390.png"
+  },
+  {
+    "revision": "1edcccbbc422405256b15d4174696142",
+    "url": "/static/media/noteTakerThumbnail.1edcccbb.png"
+  },
+  {
+    "revision": "41d44be1cbeef280237019e743efe7a4",
+    "url": "/static/media/theMixxThumbnail.41d44be1.png"
+  },
+  {
+    "revision": "84c9b8ef906b6483ae7b1ed72ea89ee9",
+    "url": "/static/media/toolNextDoorThumbnail.84c9b8ef.png"
+  }
+]);
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import ContactForm from "../components/ContactForm";
 
 export default class Contact extends Component {
